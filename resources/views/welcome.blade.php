91eb17dfabfab8daec4b6da3d00cf5511cd618da
fajeg <jegufabien@gmail.com>
2018-07-10 00:08:16 +0200

    m

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,16 +79,10 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    SUPER CARTE POSTAL 4000
                 </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                <p>
+                ceci est une api de qualité supérieure</p>
             </div>
         </div>
     </body>
